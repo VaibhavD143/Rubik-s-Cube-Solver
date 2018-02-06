6165f4511563f683f19eda4118af7bcd6eaaf72a
VaibhavD143 <vaibhav.dodiya143vd@gmail.com>
2018-02-07 01:16:30 +0530

    Phase one partially

---
+++ sample_input.py
@@ -0,0 +1,8 @@
+two bad edges 3,5
+def __init__(self):
+		self.front = ['g','w','r','r','b','g','o','g','b']
+		self.back = ['b','g','b','o','g','r','g','y','r']
+		self.left = ['o','r','r','w','y','b','g','b','y']
+		self.right = ['y','w','r','w','w','g','o','y','w']
+		self.up = ['w','r','w','y','r','o','y','b','b']
+		self.down = ['g','y','y','o','o','o','w','b','o']
--- solver.py
+++ solver.py
@@ -1,231 +1,171 @@
-class mycube():
-	def __init__(self):
-		self.front = ['w','b','y','w','b','r','y','g','o']
-		self.back = ['g','y','o','w','g','o','b','b','y']
-		self.left = ['b','r','g','g','y','o','g','b','g']
-		self.right = ['b','y','w','g','w','g','b','o','w']
-		self.up = ['y','b','r','w','r','r','o','o','r']
-		self.down = ['o','y','w','w','o','y','r','r','r']
-
-
-	def rotate(self,face,up,right,down,left,name):
-		r_face = face[:]
-		r_up = up[:]
-		r_right = right[:]
-		r_down = down[:]
-		r_left = left[:]
-		
-		
-		index = { 'f' : [[6,7,8],[0,3,6],[2,1,0],[8,5,2]],
-				  'b' : [[2,1,0],[0,3,6],[6,7,8],[8,5,2]],
-				  'l' :	[[0,3,6],[0,3,6],[0,3,6],[8,5,2]],
-				  'r' : [[8,5,2],[0,3,6],[8,5,2],[8,5,2]],
-				  'u' : [[2,1,0],[2,1,0],[2,1,0],[2,1,0]],
-				  'd' : [[6,7,8],[6,7,8],[6,7,8],[6,7,8]]
-				}
-
-		#update face side of rotation 
-		r_face[0] = face[6]
-		r_face[1] = face[3]
-		r_face[2] = face[0]
-		r_face[3] = face[7]
-		r_face[4] = face[4]
-		r_face[5] = face[1]
-		r_face[6] = face[8]
-		r_face[7] = face[5]
-		r_face[8] = face[2]
-		# print(face)
-		# print(r_face)
-		#update other four sides
-		if name == 'b':
-			loop = index['b']
-		elif name == 'f':
-			loop = index['f']
-		elif name == 'r':
-			loop = index['r']
-		elif name == 'l':
-			loop = index['l']
-		elif name == 'u':
-			loop = index['u']
-		elif name == 'd':
-			loop = index['d']
-
-		r_up[loop[0][0]] = left[loop[3][0]] 
-		r_up[loop[0][1]] = left[loop[3][1]] 
-		r_up[loop[0][2]] = left[loop[3][2]]
-
-		r_right[loop[1][0]] = up[loop[0][0]] 
-		r_right[loop[1][1]] = up[loop[0][1]] 
-		r_right[loop[1][2]] = up[loop[0][2]]
-
-		r_down[loop[2][0]] = right[loop[1][0]]
-		r_down[loop[2][1]] = right[loop[1][1]]
-		r_down[loop[2][2]] = right[loop[1][2]]
-
-		r_left[loop[3][0]] = down[loop[2][0]]
-		r_left[loop[3][1]] = down[loop[2][1]]
-		r_left[loop[3][2]] = down[loop[2][2]]
-
-		return r_face,r_up,r_right,r_down,r_left
-
-	def r(self):
-		# print(self.right)
-		self.right,self.up,self.back,self.down,self.front=self.rotate(self.right,self.up,self.back,self.down,self.front,'r')
-		# print(self.right)
-
-	def print_cube(self):
-		print('Current State of cube:')
-		print('front:',self.front)
-		print('Back: ',self.back)
-		print('left: ',self.left)
-		print('right:',self.right)
-		print('up:   ',self.up)
-		print('down: ',self.down)
-
-	def f(self):
-		self.front,self.up,self.right,self.down,self.left=self.rotate(self.front,self.up,self.right,self.down,self.left,'f')
-
-	def f1(self):
-		self.front,self.up,self.right,self.down,self.left=self.rotate(self.front,self.up,self.right,self.down,self.left,'f')
-		self.front,self.up,self.right,self.down,self.left=self.rotate(self.front,self.up,self.right,self.down,self.left,'f')
-		self.front,self.up,self.right,self.down,self.left=self.rotate(self.front,self.up,self.right,self.down,self.left,'f')
-
-	def f2(self):
-		self.front,self.up,self.right,self.down,self.left=self.rotate(self.front,self.up,self.right,self.down,self.left,'f')
-		self.front,self.up,self.right,self.down,self.left=self.rotate(self.front,self.up,self.right,self.down,self.left,'f')
-
-	def u(self):
-		self.up,self.back,self.right,self.front,self.left=self.rotate(self.up,self.back,self.right,self.front,self.left,'u')
-
-	def u1(self):
-		self.up,self.back,self.right,self.front,self.left=self.rotate(self.up,self.back,self.right,self.front,self.left,'u')
-		self.up,self.back,self.right,self.front,self.left=self.rotate(self.up,self.back,self.right,self.front,self.left,'u')
-		self.up,self.back,self.right,self.front,self.left=self.rotate(self.up,self.back,self.right,self.front,self.left,'u')
-
-	def u2(self):
-		self.up,self.back,self.right,self.front,self.left=self.rotate(self.up,self.back,self.right,self.front,self.left,'u')
-		self.up,self.back,self.right,self.front,self.left=self.rotate(self.up,self.back,self.right,self.front,self.left,'u')
-
-	def b(self):
-		self.back,self.up,self.left,self.down,self.right=self.rotate(self.back,self.up,self.left,self.down,self.right,'b')
-
-	def b1(self):
-		self.back,self.up,self.left,self.down,self.right=self.rotate(self.back,self.up,self.left,self.down,self.right,'b')
-		self.back,self.up,self.left,self.down,self.right=self.rotate(self.back,self.up,self.left,self.down,self.right,'b')
-		self.back,self.up,self.left,self.down,self.right=self.rotate(self.back,self.up,self.left,self.down,self.right,'b')
-
-	def b2(self):
-		self.back,self.up,self.left,self.down,self.right=self.rotate(self.back,self.up,self.left,self.down,self.right,'b')
-		self.back,self.up,self.left,self.down,self.right=self.rotate(self.back,self.up,self.left,self.down,self.right,'b')
-
-	def d(self):
-		self.down,self.front,self.right,self.back,self.left=self.rotate(self.down,self.front,self.right,self.back,self.left,'d')
-
-	def d1(self):
-		self.down,self.front,self.right,self.back,self.left=self.rotate(self.down,self.front,self.right,self.back,self.left,'d')
-		self.down,self.front,self.right,self.back,self.left=self.rotate(self.down,self.front,self.right,self.back,self.left,'d')
-		self.down,self.front,self.right,self.back,self.left=self.rotate(self.down,self.front,self.right,self.back,self.left,'d')
-
-	def d2(self):
-		self.down,self.front,self.right,self.back,self.left=self.rotate(self.down,self.front,self.right,self.back,self.left,'d')
-		self.down,self.front,self.right,self.back,self.left=self.rotate(self.down,self.front,self.right,self.back,self.left,'d')
-
-	def l(self):
-		self.left,self.up,self.front,self.down,self.back=self.rotate(self.left,self.up,self.front,self.down,self.back,'l')
-
-	def l1(self):
-		self.left,self.up,self.front,self.down,self.back=self.rotate(self.left,self.up,self.front,self.down,self.back,'l')
-		self.left,self.up,self.front,self.down,self.back=self.rotate(self.left,self.up,self.front,self.down,self.back,'l')
-		self.left,self.up,self.front,self.down,self.back=self.rotate(self.left,self.up,self.front,self.down,self.back,'l')
-
-	def l2(self):
-		self.left,self.up,self.front,self.down,self.back=self.rotate(self.left,self.up,self.front,self.down,self.back,'l')
-		self.left,self.up,self.front,self.down,self.back=self.rotate(self.left,self.up,self.front,self.down,self.back,'l')
-
-	def r(self):
-		self.right,self.up,self.back,self.down,self.front=self.rotate(self.right,self.up,self.back,self.down,self.front,'r')
-
-	def r1(self):
-		self.right,self.up,self.back,self.down,self.front=self.rotate(self.right,self.up,self.back,self.down,self.front,'r')
-		self.right,self.up,self.back,self.down,self.front=self.rotate(self.right,self.up,self.back,self.down,self.front,'r')
-		self.right,self.up,self.back,self.down,self.front=self.rotate(self.right,self.up,self.back,self.down,self.front,'r')
-
-	def r2(self):
-		self.right,self.up,self.back,self.down,self.front=self.rotate(self.right,self.up,self.back,self.down,self.front,'r')
-		self.right,self.up,self.back,self.down,self.front=self.rotate(self.right,self.up,self.back,self.down,self.front,'r')
-
-	def get_corner(self,no):
-		if no == 1:
-			return self.left[2],self.up[6],self.front[0]
-		elif no == 2:
-			return self.right[0],self.up[8],self.front[2]
-		elif no == 3:
-			return self.right[6],self.down[2],self.front[8]
-		elif no == 4:
-			return self.left[8],self.down[0],self.front[6]
-		elif no == 5:
-			return self.left[0],self.up[0],self.back[2]
-		elif no == 6:
-			return self.right[2],self.up[2],self.back[0]
-		elif no == 7:
-			return self.right[8],self.down[8],self.back[6]
-		elif no == 8:
-			return self.left[6],self.down[6],self.back[8]
-		else:
-			return False,False,False
-
-	def is_bad_edge(self,no):
-		
-		x,y,z = self.get_edges(no)
-		
-		if no == 1 and (y == 'y' or y == 'w' or z == 'o' or z == 'r'):
-			return True
-		elif no == 2 and (z == 'y' or z == 'w' or x == 'o' or x == 'r'):
-			return True
-		elif no == 3 and (y == 'y' or y == 'w' or z == 'o' or z == 'r'):
-			return True
-		elif no == 4 and (z == 'y' or z == 'w' or x == 'o' or x == 'r'):
-			return True
-		elif no == 5 and (x == 'o' or x == 'r' or y == 'w' or y == 'y'):
-			return True
-		elif no == 6 and (x == 'o' or x == 'r' or y == 'w' or y == 'y'):
-			return True
-		elif no == 7 and (x == 'o' or x == 'r' or y == 'y' or y == 'w'):
-			return True
-		elif no == 8 and (x == 'o' or x == 'r' or y == 'y' or y == 'w'):
-			return True
-		elif no == 9 and (z == 'o' or z == 'r' or y == 'y' or y == 'w'):
-			return True
-		elif no == 10 and (z == 'w' or z == 'y' or x == 'o' or x == 'r'):
-			return True
-		elif no == 11 and (z == 'o' or z == 'r' or y == 'y' or y == 'w'):
-			return True
-		elif no == 12 and (z == 'w' or z == 'y' or x == 'o' or x == 'r'):
-			return True
-
-
+from notation import *
 
 
 class first_phase(mycube):
+
+	global ans
+	front_edges = [1,2,3,4]
+
+	def solve_two_bad_edges(self,bad_edges):
+		if bad_edges[0] in self.front_edges and bad_edges[1] in self.front_edges:
+			if bad_edges[0] == 1:
+				self.u()
+				ans.append('u')
+			elif bad_edges[0] == 2:
+				self.r()
+				ans.append('r')
+			elif bad_edges[0] == 3:
+				self.d()
+				ans.append('d')
+			elif bad_edges[0] == 4:
+				self.l()
+				ans.append('l')
+			self.f()
+			ans.append('f')
+
+
+		elif (bad_edges[0] in self.front_edges and bad_edges[1] not in self.front_edges) or (bad_edges[0] not in self.front_edges and bad_edges[1] in self.front_edges):
+			self.f()
+			ans.append('f')
+
+		elif (bad_edges[0] not in self.front_edges and bad_edges[1] not in self.front_edges):
+			
+			if(bad_edges[0] in [10,12] and bad_edges[1] in [10,12]):
+				self.r2()
+				ans.append('r2')
+			elif(bad_edges[0] in [5,6,9] or bad_edges[1] in [5,6,9]):
+				while not self.is_bad_edge(1):
+					self.u()
+					ans.append('u')
+			elif(bad_edges[0] in [7,8,11] or bad_edges[1] in [7,8,11]):
+				while not self.is_bad_edge(3):
+					self.d()
+					ans.append('d')
+			self.f()
+			ans.append('f')
 	
-	def get_bad_edges(self):
-		bad_edges = []
-		for i in range(1,13):
-			if self.is_bad_edge(i):
-				bad_edges.append(i)
-		print(bad_edges)
-		return bad_edges
+	# def move_edge_to_front(self,no,pos):
+		
+	# 	if(no in [10,12]):
+	# 		self.r2()
+	# 		ans.append('r2')
+	# 	elif(no in [5,6,9]):
+	# 		while not self.is_bad_edge(1):
+	# 			self.u()
+	# 			ans.append('u')
+	# 	elif(no in [7,8,11]):
+	# 		while not self.is_bad_edge(3):
+	# 			self.d()
+	# 			ans.append('d')
+		
+
+	# def move_bad_edges_to_front(self,bad_edges):
+	# 	cnt = 0
+	# 	while bad_edges[cnt] in front_edges:
+	# 		cnt+=1
 
 		
 
-if __name__ == '__main__':
-	cube = mycube()
-	# cube.print_cube()
-	# cube.d()
-	# cube.print_cube()
-	# for i in range(1,9):
-		# print(cube.get_corner(i))
-	# print(cube.right)
-	f =first_phase()
-	print(f.get_bad_edges())
+	def solve_bad_edges(self):
+		bad_edges = self.get_bad_edges()
+		len_bad_edges = len(bad_edges)
+		if  len_bad_edges % 2:
+			print(bad_edges)
+			print("Invalid State")
+			return
+		elif len_bad_edges == 2 :
+			self.solve_two_bad_edges(bad_edges)
 
+		self.move_bad_edges_to_front(bad_edges)
+
+	def set_opposite_to_ro(self,a,b):
+		flag = False
+		for i in range(1,9,2):
+			if self.up[i] != 'r' and self.up[i] != 'o':
+				flag = True
+				break
+		if flag:
+			while (self.up[a] == 'o' or self.up[a] == 'r'):
+				self.u()
+				ans.append('u')
+		else:
+			while (self.down[b] =='o' or self.down[b] =='r'):
+				self.d()
+				ans.append('d')	
+
+	def make_ro_cross(self):
+		if(self.front[5]=='r' or self.front[5]=='o'):
+			self.set_opposite_to_ro(1,7)
+			if ans[-1] == 'u':
+				self.r()
+				self.u()
+				self.r1()
+				ans.append(['r','u','r1'])
+			elif ans[-1] == 'd':
+				self.r1()
+				self.d1()
+				self.r()
+				ans.append(['r1','d1','r'])
+
+		if self.front[3]=='r' or self.front[3]=='o':
+			self.set_opposite_to_ro(1,7)
+			if ans[-1] == 'u':
+				self.l1()
+				self.u1()
+				self.l()
+				ans.append(['l1','u1','l'])
+			elif ans[-1] == 'd':
+				self.l()
+				self.d()
+				self.l1()
+				ans.append(['l','d','l1'])
+		if (self.back[3]=='r' or self.back[3]=='o'):
+			self.set_opposite_to_ro(7,1)	
+			if ans[-1] == 'u':
+				self.r1()
+				self.u1()
+				self.r()
+				ans.append(['r1','u1','r'])
+			elif ans[-1] == 'd':
+				self.r()
+				self.d()
+				self.r1()
+				ans.append(['r','d','r1'])
+		if self.back[5]=='r' or self.back[5]=='o':
+			# print('in',ans)
+			self.set_opposite_to_ro(7,1)
+			if ans[-1] == 'u':
+				self.l()
+				self.u()
+				self.l1()
+				ans.append(['l','u','l1'])
+			elif ans[-1] == 'd':
+				self.l1()
+				self.u1()
+				self.l()
+				ans.append(['r1','d1','r'])
+
+if __name__ == '__main__':
+	ans = []
+	# cube = mycube()
+	f =first_phase()
+	f.u1()
+	f.r()
+	f.u()
+	f.r1()
+	f.u1()
+	f.l1()
+	f.u1()
+	f.l()
+	f.make_ro_cross()
+	print(ans)
+	# print(f.get_bad_edges())
+	# f.l1()
+	# f.d()
+	# f.r1()
+
+	# print(f.get_bad_edges())
+	# f.print_cube()
+	# print(f.solve_bad_edges())
+	# while(len(f.get_bad_edges())):
+	# 	f.solve_bad_edges()
