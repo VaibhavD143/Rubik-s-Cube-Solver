683d387cf042a62ca80b6dde74ba1ee7c08c9dd8
Sufiyan1997 <sufigani@gmail.com>
2018-03-12 20:02:18 +0530

    3b started

--- third_phase.py
+++ third_phase.py
@@ -46,3 +46,73 @@ class third_phase(mycube):
 		
 		self.set_corner_of_face(self.front,self.back,self.u,self.d,self.l,self.r,self.f,self.b,{'u':'u','d':'d','l':'l','r':'r','f':'f','b':'b'})
 		self.set_corner_of_face(self.right,self.left,self.u,self.d,self.f,self.b,self.r,self.l,{'u':'u','d':'d','l':'f','r':'b','f':'r','b':'l'})
+
+	def check_algo2(self):
+		edge_move = [{1:self.d2,3:self.r2},{1:self.d2,3:self.b2},{1:self.f2,3:self.r2}]
+		face_function_name = {self.front:'f2',self.back:'b2',self.left:'l2',self.right:'r2',self.up:'u2',self.down:'d2'}
+		edge_middle_move = [{1:1,3:3},{1:2,3:3},{1:1,3:2}]
+		i = 0
+		for f,b in face_pairs.items:
+			if f[1] != face_color[f] and b[1] != face_color[b]:
+				if f[7] != face_color[f] and b[7] != face_color[b]:
+					self.algo2(edge_move[i][1],face_function_name[edge_move[i][1]],edge_middle_move[i][1])
+			elif f[3] != face_color[f] and b[3] != face_color[b]:
+				if f[5] != face_color[f] and b[5] != face_color[b]:
+					self.algo2(edge_move[i][3],face_function_name[edge_move[i][3]],edge_middle_move[i][3])
+	def check_algo4(self):
+
+		if self.front[1]!=self.front[4] and self.right[1]!=self.right[4] and self.back[1]!=self.back[4] and self.left[1]!=self.left[4]:
+			self.algo4()
+		elif self.front[7]!=self.front[4] and self.right[7]!=self.right[4] and self.back[7]!=self.back[4] and self.left[7]!=self.left[4]:
+
+		elif self.front[5]!=self.front[4] and self.up[5]!=self.up[4] and self.back[3]!=self.back[4] and self.down[5]!=self.down[4]:
+
+		elif self.front[3]!=self.front[4] and self.up[3]!=self.up[4] and self.back[5]!=self.back[4] and self.down[3]!=self.down[4]:
+
+		elif self.right[3]!=self.right[4] and self.up[7]!=self.up[4] and self.left[5]!=self.left[4] and self.down[1]!=self.down[4]:
+
+		elif self.right[5]!=self.right[4] and self.up[1]!=self.up[4] and self.left[3]!=self.left[4] and self.down[7]!=self.down[4]:
+
+	def algo1(self,up_fun,right_fun,up_fun_name,right_fun_name):
+		for x in range(0,3):
+			up_fun()
+			self.ans.append(up_fun_name)
+			right_fun()
+			self.ans.append(right_fun_name)
+	def algo2(self,front_fun,front_fun_name,middle_layer):
+		for x in range(0,2):
+			front_fun()
+			self.ans.append(front_fun_name)
+			
+			if middle_layer == 1:
+				self.mf2()
+				self.ans.append('mf2')
+			elif middle_layer == 2:
+				self.mr2()
+				self.ans.append('mr2')
+			elif middle_layer == 3:
+				self.mc2()
+				self.ans.append('mc2')
+	def algo3(self,front_fun,front_fun_name):
+		front_fun()
+		self.r1()
+		self.l()
+		front_fun()
+		self.r()
+		self.l1()
+		self.ans.extend([front_fun_name,'r1','l',front_fun_name,'r','l1'])
+		
+	def algo4(self,up_fun,up_fun_name,middle_layer):
+		self.l2()
+		self.r2()
+		up_fun()
+		self.l2()
+		self.r2()
+		up_fun()
+		up_fun()
+		self.l2()
+		self.r2()
+		up_fun()
+		self.l2()
+		self.r2()
+		self.ans.extend(['l2','r2',up_fun_name,'l2','r2',up_fun_name,up_fun_name,'l2','r2',up_fun_name,'l2','r2'])
